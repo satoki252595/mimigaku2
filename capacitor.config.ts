28461c7327906cd6e556d882e09598888ab169ca
satoki252595 <satoki252595@gmail.com>
2024-07-06 21:13:55 +0900

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'mimigaku2',
+  webDir: 'build'
+};
+
+export default config;
